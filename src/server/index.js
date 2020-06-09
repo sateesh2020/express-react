7b05152cc98cfc1f039e87b893790c0ffed8c42b
Satheesh Valluru <vallurusateesh2009@gmail.com>
2020-06-09 14:43:11 +0530

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,64 @@
+const express = require('express'); // call express
+
+const app = express(); // define our app using express
+const bodyParser = require('body-parser');
+const os = require('os');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 8050; // set our port
+
+app.use(express.static('dist'));
+app.get('/', function (req, res) {
+  console.log('sending index.html');
+  res.sendFile('/dist/index.html');
+});
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+  // do logging
+  console.log('App is running');
+  next(); // make sure we go to the next routes and don't stop here
+});
+// test http methods
+// ----------------------------------------------------
+router
+  .route('/test')
+  .get(function (req, res) {
+    res.json({ username: os.userInfo().username });
+  })
+  .post(bodyParser.json(), function (req, res) {
+    res.json(req.body);
+  })
+  .put(bodyParser.json(), function (req, res) {
+    res.json(req.body);
+  })
+  .delete(bodyParser.json(), function (req, res) {
+    res.json(req.body);
+  });
+
+router
+  .route('/hooks')
+  .get(function (req, res) {
+    console.log(req.headers);
+    res.json({ verification: req.headers['X-Okta-Verification-Challenge'] });
+  })
+  .post(function (req, res) {
+    console.log(req.body);
+    res.json({ message: 'Hooks received' });
+  });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log(`App listening on ${port}`);
